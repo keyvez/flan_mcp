fb6617c5ea1181a6b7cd22bc9f1742c8ba6a475e
Gaurav Misra <keyvez@gmail.com>
2026-03-15 23:17:10 -0700

    chore: Minor example app UI tweaks and add Podfiles

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -125,10 +125,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: .center,
-          spacing: 24,
+          spacing: 16,
           children: [
             Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times',
               style: TextStyle(
                 fontFamily: 'Georgia',
                 fontSize: 28,
